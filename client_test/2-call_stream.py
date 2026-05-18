bf1f5a296857dffe5de7abe52a6a8f7cc112a197
Your Name <you@example.com>
2026-05-18 17:54:09 +0800

    测试接通模型

---
+++ client_test/2-call_stream.py
@@ -0,0 +1,23 @@
+from openai import OpenAI
+
+client = OpenAI(
+    api_key="sk-CKHWDcpDc17l0kqF53C91f1fE907493aA13a01Ee55A6F1C2",
+    base_url="http://localhost:3000/v1"
+)
+
+stream = client.chat.completions.create(
+    #model="deepseek-v4-pro",
+    model="glm-4-7-251222",
+    messages=[
+        {"role": "user", "content": "写一首关于春天的诗"}
+    ],
+    stream=True
+)
+
+for chunk in stream:
+    if chunk.choices[0].delta.content:
+        print(chunk.choices[0].delta.content, end="")
+
+
+
+
